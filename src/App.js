48a06c6c91b023dd3d4618e4f0fb964daed6d8a4
Dmitriy Troitskiy <dmitriysabbath@gmail.com>
2021-12-14 07:45:10 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import Form from './components/Form/Form';
 
 // import css from "./App.module.css";
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const INITIAL_STATE = {
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+  filter: '',
+};
 
-  handleSubmit = () => {};
+class App extends Component {
+  state = { ...INITIAL_STATE };
+
+  onFormSubmit = data => {
+    console.log(data);
+  };
 
   render() {
     return (
       <>
-        <Form />
+        <Form onSubmit={this.onFormSubmit} />
       </>
     );
   }
